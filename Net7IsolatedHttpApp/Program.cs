c6788cb1ada5ce3039ef38031dbddb0a8787f1f2
Shyju Krishnankutty <connectshyju@gmail.com>
2022-09-07 20:27:08 -0700

    AOT working app

--- Net7IsolatedHttpApp/Program.cs
+++ Net7IsolatedHttpApp/Program.cs
@@ -2,6 +2,12 @@ using Microsoft.Azure.Functions.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Net7IsolatedHttpApp;
+using System.Reflection;
+using System.Runtime.Loader;
+
+//var pathToAssembly = "C:\\Dev\\Temp\\AOTPlayground\\Net7IsolatedHttpApp\\Net7IsolatedHttpApp\\bin\\Debug\\net7.0\\Net7IsolatedHttpApp.dll";
+//Console.WriteLine("pathToAssembly1:" + pathToAssembly);
+//Assembly assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(pathToAssembly);
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
