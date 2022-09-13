804e14df98b8a575fca9ac46230647914f05d609
Shyju Krishnankutty <connectshyju@gmail.com>
2022-09-12 19:30:53 -0700

    2.0.8 package

--- Net7IsolatedHttpApp/Program.cs
+++ Net7IsolatedHttpApp/Program.cs
@@ -1,16 +1,10 @@
 using Microsoft.Azure.Functions.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Net7IsolatedHttpApp;
-using System.Reflection;
-using System.Runtime.Loader;
-
-//var pathToAssembly = "C:\\Dev\\Temp\\AOTPlayground\\Net7IsolatedHttpApp\\Net7IsolatedHttpApp\\bin\\Debug\\net7.0\\Net7IsolatedHttpApp.dll";
-//Console.WriteLine("pathToAssembly1:" + pathToAssembly);
-//Assembly assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(pathToAssembly);
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    // Use the source generated metadagenerator insetad of the reflection based one
     .ConfigureServices(s =>
     {
         s.AddSingleton<IFunctionMetadataProvider, Microsoft.Azure.Functions.Worker.GeneratedFunctionMetadataProvider>();
